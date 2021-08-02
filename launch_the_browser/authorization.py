1896bf5b0f9b9ab1fdc08cb69543ae55ca531b04
DanilPidhainyi <61890854+DanilPidhainyi@users.noreply.github.com>
2021-08-02 10:46:21 +0300

    Update authorization.py

--- launch_the_browser/authorization.py
+++ launch_the_browser/authorization.py
@@ -2,7 +2,7 @@ from time import sleep
 import re
 from selenium.common.exceptions import NoSuchElementException
 
-
+login = ''
 password = r'7acA3L2K4rEii3XU599mhs74Mg6LZ9BAz84k72zL2L' \
            'mp3X9U7r36fn6HYrE62A8ZaJk83i2c2Ry7Z5JzikScX' \
            '5B584nT695MiZ6a8h43FxSdV6N9g3Fz9aF3y3GCc778D' \
@@ -10,7 +10,7 @@ password = r'7acA3L2K4rEii3XU599mhs74Mg6LZ9BAz84k72zL2L' \
            'J4j9agS5KgLX4255NriaXL32p3E5a'
 
 
-def authorization_fb(driver, login_facebook='0681286458', pass_facebook=password, new_page=True, delay=0.1):
+def authorization_fb(driver, login_facebook=login, pass_facebook=password, new_page=True, delay=0.1):
     """Авторизація у фейсбук
     authorization_fb -> driver
     @login_facebook
